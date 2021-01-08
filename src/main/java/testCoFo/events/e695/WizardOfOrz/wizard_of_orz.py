f8d073d2b3ff19588c9adc5038ab247d07b40090
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-08 18:02:00 +0300

    preparation

---
+++ src/main/java/testCoFo/events/e695/WizardOfOrz/wizard_of_orz.py
@@ -0,0 +1,1 @@
+def wizard_of_orz():
---
+++ src/test/java/testCoFo/events/e695/WizardOfOrz/test_wizard_of_orz.py
@@ -0,0 +1,1 @@
+import pytesttest_data = []@pytest.mark.parametrize("number,input_data,expected_result",test_data)def test_wizard_of_orz(number:int,input_data:List[int],expected_result)
